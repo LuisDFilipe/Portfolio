78a7b38019ed7337ebf58bb564e79bc93b96ff60
LuisDFilipe <132720266+LuisDFilipe@users.noreply.github.com>
2025-07-20 16:02:40 +0100

    index

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("my-pwa-cache").then((cache) => {
-      return cache.addAll(["/index.html", "assets/icon_192.png"]);
+      return cache.addAll(["index.html", "assets/icon_192.png"]);
     }).catch(err => {
       console.error('Caching failed:', err);
     })
